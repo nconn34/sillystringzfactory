7a11ce0f11c9497b01769bab96203be8d15331e7
Nathan Conn <nconn34@gmail.com>
2022-03-26 14:36:13 -0700

    Fixed spelling errors

--- Models/DesignTimeDbContextFactory.cs
+++ Models/DesignTimeDbContextFactory.cs
@@ -5,7 +5,7 @@ using System.IO;
 
 namespace Factory.Models
 {
-  public class ToDoListContextFactory : IDesignTimeDbContextFactory<FactoryContext>
+  public class FactoryContextFactory : IDesignTimeDbContextFactory<FactoryContext>
   {
 
     FactoryContext IDesignTimeDbContextFactory<FactoryContext>.CreateDbContext(string[] args)
